942671648767af86a4050fbb5e81c7cd0ac1947e
vdmetran <vdmetran2@sjcgroup.ph>
2021-04-17 08:33:26 +0800

    Create Migrations (not done)

---
+++ database/migrations/2021_04_17_001652_create_tbl_users.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('api_token', 80)
+                    ->unique()
+                    ->nullable()
+                    ->default(null);
+            $table->rememberToken();
+            $table->timestamps();
+            $table->dropSoftDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_users');
+    }
+}
